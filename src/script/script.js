05939c2cd7135d8e7d719a43fe7b695aeeb03abc
TheDarkStrix <thedarkstrix@gmail.com>
2022-03-31 15:49:19 +0530

    Add player and enemy

--- src/script/script.js
+++ src/script/script.js
@@ -4,3 +4,67 @@ const canvasContext = canvas.getContext("2d");
 canvas.width = 1024;
 canvas.height = 576;
 canvasContext.fillRect(0, 0, canvas.width, canvas.height);
+
+const gravity = 0.2;
+class Sprite {
+  constructor({ position, velocity }) {
+    this.position = position;
+    this.velocity = velocity;
+    this.height = 150;
+  }
+
+  draw() {
+    canvasContext.fillStyle = "red";
+    canvasContext.fillRect(this.position.x, this.position.y, 50, this.height);
+  }
+
+  update() {
+    this.draw();
+    this.position.y += this.velocity.y;
+    /** this.position.y + this.height + this.velocity.y is bottom end of the player
+     */
+    if (this.position.y + this.height + this.velocity.y >= canvas.height) {
+      /** set the velocity to 0 when player/ememy reacher the bottom of the canvas */
+      this.velocity.y = 0;
+    } else {
+      /** Update the gravity if the player has not yet reached the bottom of the canvas */
+      this.velocity.y += gravity;
+    }
+  }
+}
+
+const player = new Sprite({
+  position: {
+    x: 0,
+    y: 0,
+  },
+  velocity: {
+    x: 0,
+    y: 10,
+  },
+});
+
+const enemy = new Sprite({
+  position: {
+    x: 400,
+    y: 50,
+  },
+  velocity: {
+    x: 0,
+    y: 0,
+  },
+});
+
+player.update();
+enemy.update();
+
+function animate() {
+  window.requestAnimationFrame(animate);
+  /** Reset the canvas on every frame */
+  canvasContext.fillStyle = "black";
+  canvasContext.fillRect(0, 0, canvas.width, canvas.height);
+  player.update();
+  enemy.update();
+}
+
+animate();
